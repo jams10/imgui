5f76abde51159e61ac827295863d0e81289948c5
jams10 <52204522+jams10@users.noreply.github.com>
2023-07-28 10:24:04 +0900

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "ImGui"
   kind "StaticLib"
   language "C++"
+  staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -22,8 +23,11 @@ project "ImGui"
 
   filter "system:windows"
 	  systemversion "latest"
-	  cppdialect "C++17"
-    staticruntime "On"
 
-  filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
